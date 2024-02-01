537354a607b2519010d962994895d53692ae6191
MonkeeeCs <114259817+MonkeeeCs@users.noreply.github.com>
2024-02-01 12:34:50 +0100

    Main files
    Adding in main files of the project.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Project Information
+This project has been made for educational porpuses only and will be available [here on this website](https://monkeee-cs.github.io/fizika/)!
+
+# Usage:
+If you really want you can install this files to your pc!
+## Widnows:
+1. click widnows + r on your keybooard,
+2. enter `cmd` and click okay or just click enter,
+3. in to the cmd enter
+```git clone https://github.com/monkeee-cs/fizika```
+**NOTE:** You will need to install [git](https://git-scm.com/) for this to work!
+## MacOS:
+1. [Open terminal](https://support.apple.com/sl-si/guide/terminal/apd5265185d-f365-44cb-8b09-71a064a42125/mac) on your Mac,
+2. Enter this command in yor terminal:
+```sudo apt install git```
+3. after that go where you want to download this files using  `cd <path>` in the terminal,
+4. Use this command to download/clone files:
+```git clone https://github.com/monkeee-cs/fizika```
+## Mobile:
+Just go to the link in *Project Information*.
+# Note
+This is totally free and does not cost anything so if u wanna use source code be my guest but please do not claim it as your own!
---
+++ main.js
@@ -0,0 +1,112 @@
+function vel() {
+    const s = document.getElementById('sp1text');
+    const t = document.getElementById('ti1text');
+    const typeSpe = document.getElementById('typeSpe');
+    const typeTim = document.getElementById('typeTim');
+    if (typeSpe.value === 'meter' && typeTim.value === 'sec') {
+        const result =  `v = ${s.value / t.value} m/s`;
+        document.getElementById('vel1ans').innerHTML = result;
+    } else if (typeSpe.value === 'km' && typeTim.value === 'h') {
+        const result =  `v = ${s.value / t.value} km/h`;
+        document.getElementById('vel1ans').innerHTML = result;
+    } else {
+        alert('Please, for M/S use Meter and Seconds and for Km/H use Kilometer and Hour')
+    };
+};
+function time() {
+    const s = document.getElementById('sp2text');
+    const v = document.getElementById('vel1text');
+    const typeSpe = document.getElementById('typeSpe1');
+    const typeVel = document.getElementById('typeVel');
+    if (typeSpe.value === 'km' && typeVel.value === 'kmh') {
+        const result = `t = ${s.value / v.value} h`;
+        document.getElementById('ti1ans').innerHTML = result;
+    } else if (typeSpe.value === 'meter' && typeVel.value === 'ms') {
+        const result = `t = ${s.value / v.value} s`;
+        document.getElementById('ti1ans').innerHTML = result;
+    } else {
+        alert('Please, for t[s] use seconds and M/s and for t[h] use Kilometers and Km/h')
+    };
+};
+function speed() {
+    const t = document.getElementById('ti2text');
+    const v = document.getElementById('vel2text');
+    const typeTim = document.getElementById('typeTim1');
+    const typeVel = document.getElementById('typeVel1');
+    if (typeTim1.value === 'sec' && typeVel.value === 'ms') {
+        const result = `s = ${t.value * v.value} Meters`;
+        document.getElementById('sp1ans').innerHTML = result;
+    } else if (typeTim.value === 'h' && typeVel.value === 'kmh') {
+        const result = `s = ${t.value * v.value} Kilometers`;
+        document.getElementById('sp1ans').innerHTML = result;
+    } else {
+        alert('Please, for s[m] use Meters and Seconds and for s[km] use Kilometers and hours (im not that smart)');
+    };
+};
+function kmh() {
+    const ms = document.getElementById('ms1');
+    const result = `${ms.value * 3.6} Km/h`;
+    document.getElementById('kmh1').innerHTML = result;
+};
+function ms() {
+    const kmh = document.getElementById('kmh2');
+    const result = `${kmh.value / 3.6} M/s`;
+    document.getElementById('ms2').innerHTML = result;
+};
+function wikiVel() {
+    window.open('https://en.wikipedia.org/wiki/Velocity', '_blank');
+};
+function wikiTime() {
+    window.open('https://en.wikipedia.org/wiki/Time_in_physics', '_blank');
+};
+function wikiDis() {
+    window.open('https://www.khanacademy.org/science/physics/one-dimensional-motion/displacement-velocity-time/a/what-is-displacement', '_blank');
+};
+function wikiMs() {
+    window.open('https://www.techtarget.com/whatis/definition/meter-per-second-m-s-or-m-sec', '_blank');
+};
+function wikiKmh() {
+    window.open('https://en.wikipedia.org/wiki/Kilometres_per_hour', '_blank');
+};
+
+
+/** 
+
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░▄▄▄▄▄▄▄░░░░░░░░░
+░░░░░░░░░▄▀▀▀░░░░░░░▀▄░░░░░░░
+░░░░░░░▄▀░░░░░░░░░░░░▀▄░░░░░░
+░░░░░░▄▀░░░░░░░░░░▄▀▀▄▀▄░░░░░
+░░░░▄▀░░░░░░░░░░▄▀░░██▄▀▄░░░░
+░░░▄▀░░▄▀▀▀▄░░░░█░░░▀▀░█▀▄░░░
+░░░█░░█▄▄░░░█░░░▀▄░░░░░▐░█░░░
+░░▐▌░░█▀▀░░▄▀░░░░░▀▄▄▄▄▀░░█░░
+░░▐▌░░█░░░▄▀░░░░░░░░░░░░░░█░░
+░░▐▌░░░▀▀▀░░░░░░░░░░░░░░░░▐▌░
+░░▐▌░░░░░░░░░░░░░░░▄░░░░░░▐▌░
+░░▐▌░░░░░░░░░▄░░░░░█░░░░░░▐▌░
+░░░█░░░░░░░░░▀█▄░░▄█░░░░░░▐▌░
+░░░▐▌░░░░░░░░░░▀▀▀▀░░░░░░░▐▌░
+░░░░█░░░░░░░░░░░░░░░░░░░░░█░░
+░░░░▐▌▀▄░░░░░░░░░░░░░░░░░▐▌░░
+░░░░░█░░▀░░░░░░░░░░░░░░░░▀░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ 
+
+
+░░░░░░░░░░░░░░░░░░░░░░█████████░░░░░░░░░
+░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███░░░░░░░
+░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███░░░░
+░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░
+░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███░
+░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██
+░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██
+░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██
+██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░
+░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░
+░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░░░░
+░░████████████░░░█████████████████░░░░░░
+
+**/
